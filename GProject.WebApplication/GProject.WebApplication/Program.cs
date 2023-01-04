31d0fd8c48174910f5a2331517d94b09b226d8e9
vu duc nham <vuducnham1123@gmail.com>
2023-01-04 07:19:26 +0700

    Commit

--- GProject.WebApplication/GProject.WebApplication/Program.cs
+++ GProject.WebApplication/GProject.WebApplication/Program.cs
@@ -12,16 +12,18 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseAuthentication();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseSession();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.Run();
