d6c5f2348f229d94db46ac88e44c036b18d1f73e
vuducnham1123 <vuducnham1123@gmail.com>
2023-04-23 22:51:25 +0700

    commit

---
+++ GProject.WebApplication/GProject.Data/Migrations/20230422165428_updateEntityVoucher.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GProject.Data.Migrations
+{
+    public partial class updateEntityVoucher : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaximumDiscount",
+                table: "Vouchers",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaximumDiscount",
+                table: "Vouchers");
+        }
+    }
+}
