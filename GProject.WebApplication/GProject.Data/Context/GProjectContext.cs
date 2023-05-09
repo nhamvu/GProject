d0936a91896522b3962f956bdac1f0866b8cdf46
NTQ-SOLUTION\lam.doan <luuhuy220902@gmail.com>
2023-05-09 18:14:18 +0700

    a

--- GProject.WebApplication/GProject.Data/Context/GProjectContext.cs
+++ GProject.WebApplication/GProject.Data/Context/GProjectContext.cs
@@ -19,7 +19,7 @@ namespace GProject.Data.Context
             //@"Data Source=DESKTOP-VVGQQLR;Initial Catalog=DB_DATNChuan;Integrated Security=True"
             //@"Data Source=LAPTOP-2GTIBL55\SQLEXPRESS;Initial Catalog=ABC;Integrated Security=True"
 
-            optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=DB_DATNChuan;Integrated Security=True");
+            optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=ABCD;Integrated Security=True");
 
 
             //DB_Du_An
